3ef3f36d4e52fb04ad92c3133f2686c635b1762b
dhoni <dhoni@kanosolution.com>
2024-08-09 10:40:46 +0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import ContactMe from './components/ContactMe.vue'
 </script>
 
 <template>
-  <div class="mb-4">
+  <div class="">
     <HomeView />
     <section id="about">
       <Profile />
@@ -21,6 +21,28 @@ import ContactMe from './components/ContactMe.vue'
       <ContactMe />
     </section>
   </div>
+  
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="flex justify-end">
+      &copy;2024 Dhoni
+      <a href="https://github.com/doniarifin"
+          class="
+            hover:scale-105
+            transition
+            duration-300
+            ease-in-out
+            font-medium 
+            rounded-xl
+            inline-flex
+            items-center
+            justify-center
+          "
+        >
+        <mdicon name="github" width="20" class="ml-2" />
+      </a>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
@@ -35,5 +57,15 @@ import ContactMe from './components/ContactMe.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.footer {
+  text-align: center;
+  font-weight: 300;
+  padding: 1rem;
+  background-color: var(--dark);
+  color: whitesmoke;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
 }
 </style>
