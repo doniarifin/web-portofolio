7e07025dde94c96aaac5a6bfb30f7b5b6725a21f
Dhoni <muh.dhoni@gmail.com>
2025-08-28 20:32:35 +0700

    fix footer

--- src/menu/Footer.vue
+++ src/menu/Footer.vue
@@ -3,23 +3,13 @@
     class="max-w-6xl mx-auto px-6 pt-10 flex flex-col md:flex-row items-center justify-between"
   >
     <div class="items-center text-gray-300">Handcrafted by Doni</div>
-    <div class="text-gray-300 text-xl flex gap-2">
+    <div class="text-gray-300 text-lg flex gap-2">
       <div class="hover:scale-105 transition duration-300 ease-in-out">
-        <a
-          href="https://github.com/doniarifin"
-          target="_blank"
-          class="font-medium rounded-xl flex items-center justify-center"
-        >
-          GitHub
-        </a>
+        <a href="https://github.com/doniarifin" target="_blank"> GitHub </a>
       </div>
       |
       <div class="hover:scale-105 transition duration-300 ease-in-out">
-        <a
-          href="https://github.com/doniarifin"
-          target="_blank"
-          class="font-medium rounded-xl flex items-center justify-center"
-        >
+        <a href="https://www.linkedin.com/in/mdhoni-zahrul-am/" target="_blank">
           LinkedIn
         </a>
       </div>
